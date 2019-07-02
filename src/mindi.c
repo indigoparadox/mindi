a1d2d611d61004509ad89d97ff1c82e9c741b534
indigoparadox <indigo.repo@interfinitdynamics.info>
2019-07-01 21:49:14 -0400

    Can iterate chunks in memory stream.

--- src/mindi.c
+++ src/mindi.c
@@ -1,35 +1,88 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <assert.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
-void read_msb_first( FILE* stream, int32_t* out ) {
-   uint8_t* byte_ptr = (uint8_t*)out;
+struct midi_chunk {
+   char type[5];
+   int32_t len;
+   uint8_t data[];
+} __attribute__( (packed) );
+
+/** \brief  Given an int32 stored as MSB-first, give a platform-appropriate
+ *          int32.
+ */
+int32_t read_int32_msb_first( int32_t* num ) {
+   int32_t out = 0;
+   uint8_t* bytes = (uint8_t*)num;
    int i;
  
-   for( i = 3 ; 0 <= i ; i-- ) {
-      fread( &(byte_ptr[i]), 1, 1, stream );
+   for( i = 0 ; 3 > i ; i++ ) {
+      out <<= 8;
+      out |= bytes[i];
    }
+
+   return out;
 }
 
-void read_chunk( ) {
+/** \brief  Given a chunk, get the chunk following it in the MIDI stream.
+ */
+struct midi_chunk* next_chunk( struct midi_chunk* chunk ) {
+   int32_t chunk_len = 0;
+   uint8_t* byte_ptr = (uint8_t*)chunk;
 
+   chunk_len = read_int32_msb_first( &(chunk->len) );
+
+   /* Subtract 1 for the dummy data ptr. */
+   return (struct midi_chunk*)
+      (byte_ptr + sizeof( struct midi_chunk ) + chunk_len - 1 );
 }
 
 int main( int argc, char** argv ) {
-   FILE* midi = NULL;
+   int midi_handle = 0;
    char* filename = "creator.mid";
-   char chunk_type[5] = { 0 };
    int32_t chunk_len = 0;
+   struct midi_chunk* chunk = NULL;
+   size_t filesize = 0;
+   struct stat st;
+   uint8_t* midi_map = NULL;
+   size_t bytes_iter = 0;
   
-   midi = fopen( filename, "rb" );
-   fread( chunk_type, 1, 4, midi );
-   read_msb_first( midi, &chunk_len );
+   stat( filename, &st );
+   filesize = st.st_size;
 
-   printf( "Chunk: Type \"%s\", %d bytes long.\n", chunk_type, chunk_len );
+   midi_handle = open( filename, O_RDONLY, 0 );
+   assert( 0 < midi_handle );
 
+   midi_map = mmap( NULL, filesize, PROT_READ, MAP_PRIVATE | MAP_POPULATE,
+      midi_handle, 0 );
+   assert( MAP_FAILED != midi_map );
 
+   /* Start iterating chunks. */
+   chunk = (struct midi_chunk*)midi_map;
+   chunk_len = read_int32_msb_first( &(chunk->len) );
+   do {
+      printf( "Chunk: Type \"%s\", %d bytes long.\n",
+         chunk->type, chunk->len );
 
-   fclose( midi );
+      /* Iterate the next chunk. */
+      bytes_iter += chunk_len;
+      bytes_iter += sizeof( struct midi_chunk ) - 1;
+      chunk = next_chunk( chunk );
+      chunk_len = read_int32_msb_first( &(chunk->len) );
+   } while( 0 < chunk_len && bytes_iter < filesize );
+
+   if( 0 < midi_handle ) {
+      close( midi_handle );
+   }
+
+   if( NULL != midi_map ) {
+      munmap( midi_map, filesize );
+   }
 
    return 0;
 }
