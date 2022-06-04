c8f935f250fb967fbcaafdd9521b6daafbd9b880
indigoparadox <indigo.repo@interfinitydynamics.info>
2022-06-04 17:49:01 -0400

    Improved timing somewhat.

--- src/midibeep.c
+++ src/midibeep.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 #ifdef USE_ALSA
+#include <time.h>
 #include <alsa/asoundlib.h>
 #elif defined( USE_DOS )
 #include <dos.h>
@@ -20,13 +21,36 @@
 
 #define MIDI_PARAMS_SZ 10
 
+#define NOTE_MS 200
+
 /* const float gc_midi_tones[128] = { 8.18, 8.66, 9.18, 9.72, 10.30, 10.91, 11.56, 12.25, 12.98, 13.75, 14.57, 15.43, 16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, 7458.62, 7902.13, 8372.02, 8869.84, 9397.27, 9956.06, 10548.08, 11175.30, 11839.82, 12543.85 }; */
 
 const int gc_midi_tones[128] = { 8, 8, 9, 9, 10, 10, 11, 12, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 23, 24, 25, 27, 29, 30, 32, 34, 36, 38, 41, 43, 46, 49, 51, 55, 58, 61, 65, 69, 73, 77, 82, 87, 92, 98, 103, 110, 116, 123, 130, 138, 146, 155, 164, 174, 185, 196, 207, 220, 233, 246, 261, 277, 293, 311, 329, 349, 369, 392, 415, 440, 466, 493, 523, 554, 587, 622, 659, 698, 739, 783, 830, 880, 932, 987, 1046, 1108, 1174, 1244, 1318, 1396, 1479, 1567, 1661, 1760, 1864, 1975, 2093, 2217, 2349, 2489, 2637, 2793, 2959, 3135, 3322, 3520, 3729, 3951, 4186, 4434, 4698, 4978, 5274, 5587, 5919, 6271, 6644, 7040, 7458, 7902, 8372, 8869, 9397, 9956, 10548, 11175, 11839, 12543 };
 
 #ifdef USE_ALSA
 snd_pcm_t* g_pcm = NULL;
+uint32_t g_s_launch = 0;
 #endif /* USE_ALSA */
+
+uint32_t get_ms() {
+#ifdef USE_ALSA
+   struct timespec spec;
+   uint32_t ms_out = 0,
+      ms_launch_delta = 0;
+   
+   clock_gettime( CLOCK_MONOTONIC, &spec );
+
+   /* Get the seconds since program launched. Multiply by 1000, so we want a
+    * smaller number than seconds since the epoch. */
+   ms_launch_delta = spec.tv_sec - g_s_launch;
+   ms_out += ms_launch_delta * 1000;
+   ms_out += spec.tv_nsec / 1000000;
+
+   return ms_out;
+#elif defined( USE_DOS )
+
+#endif /* USE_ALSA || USE_DOS */
+}
 
 void beep( int freq_hz, int duration_ms ) {
 #ifdef USE_ALSA
@@ -49,7 +73,7 @@ void beep( int freq_hz, int duration_ms ) {
    }
 
    /* Duration here kinda chosen at random by what sounds right-ish. */
-   for( i = 0 ; duration_ms / 1500 > i ; i++ ) {
+   for( i = 0 ; duration_ms / 150 > i ; i++ ) {
       snd_pcm_prepare( g_pcm );
       for( j = 0 ; 2400 > j ; j++ ) {
          buffer[j] = freq_hz > 0 ? (255 * j * freq_hz / 8000) : 0;
@@ -111,7 +135,8 @@ int main( int argc, char** argv ) {
    uint16_t ticks_per_qrtr = 48;
    uint32_t us_per_qrtr = 500000,
       us_per_tick = 0,
-      evt_time = 0;
+      evt_time = 0,
+      event_ms = 0;
 
    if( 3 > argc ) {
       fprintf( stderr, "usage: %s <midi file> <track number>\n", argv[0] );
@@ -155,11 +180,14 @@ int main( int argc, char** argv ) {
    ticks_per_qrtr = mindi_header_tdiv( midi_bytes );
    us_per_tick = us_per_qrtr / ticks_per_qrtr;
 
+   printf( "microseconds per quarter: %d\n", us_per_qrtr );
    printf( "ticks per quarter: %d\n", ticks_per_qrtr );
+   printf( "microseconds per tick: %d\n", us_per_tick );
 
    do {
       /* Setup the track and initial event offsets. */
-      track_offset = mindi_next_chunk( midi_bytes, midi_bytes_sz, track_offset );
+      track_offset = 
+         mindi_next_chunk( midi_bytes, midi_bytes_sz, track_offset );
       track_sz = mindi_chunk_sz( midi_bytes, track_offset );
       evt_offset = track_offset;
 
@@ -186,9 +214,16 @@ int main( int argc, char** argv ) {
 
          if( 0 < freq_hz_prev ) {
             /* TODO: Play the previous event until this one. */
-            printf( "beep at %d hz for %d ms\n",
-               freq_hz_prev, evt_time * us_per_tick );
-            beep( freq_hz_prev, evt_time * us_per_tick );
+            event_ms = (evt_time * us_per_tick) / 10;
+            printf( "beep at %d hz for %d ms\n", freq_hz_prev, NOTE_MS );
+            beep( freq_hz_prev, NOTE_MS );
+            if( NOTE_MS < event_ms ) {
+               printf( "sleep for %d ms\n", event_ms - NOTE_MS );
+#ifdef USE_ALSA
+               usleep( (event_ms - NOTE_MS) * 1000 );
+#elif defined( USE_DOS )
+#endif /* USE_ALSA || USE_DOS */
+            }
          }
 
          /* Queue up this event. */
